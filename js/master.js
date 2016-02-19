53497e4fdc7846aa8170c4655047f89f92204b16
Chapman <chapman@apextion.com>
2016-02-19 13:48:31 -0500

    Fresh info, missin 1

--- js/master.js
+++ js/master.js
@@ -4,75 +4,165 @@ angular.module("jam",['youtube-embed'])
 })
 .service("DataAccess",function(){
   var projects = [
+
     {
-      id:"blank",
-      teamName: '[Blank]',
-      projectTitle: 'Project U.S.O.R',
-      githubLink: '#',
-      downloadLink: '#',
-      youTubeID: 'IY8_ucCmQbs',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nunc turpis, volutpat id feugiat eu, auctor ac leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed tortor dolor, rhoncus ut nisl quis, malesuada efficitur ligula',
+      id:"guard",
+      teamName: 'Guardians of the Realm',
+      projectTitle: 'YaSuMi',
+      githubLink: 'https://github.com/TjayCargle/YaSuMi.git',
+      downloadLink: 'https://drive.google.com/file/d/0B80rXRuIDot2TDRhY1NoT2d5Q2s/view?usp=sharing',
+      youTubeID: 'ZR6NieeT8D0',
+      description: 'Cooperative puzzle game meant to be played alone or with a friend, solving puzzles in each world to move your ship to the next one. Each character is controlled by a joystick and have different abilities so they will need to work together to make it through the game. The controller is just two arcade joysticks, nothing else is needed, so CECO will be able to have small kids with all the varying levels of Cerebral Palsy help the small aliens on their earth vacation!',
       members: [
         {
-          name: 'Joe Slater',
-          role: 'Audio Engineering'
+          name: 'Vishwajeet Kale',
+          role: 'Level Layouts/Presentation'
         },
         {
-          name: 'Rebecca Carroll',
-          role: 'Back-End Dev'
+          name: 'Lexie Rood',
+          role: 'Level Layouts/Presentation'
         },
         {
-          name: 'Jimmy Johnson',
-          role: 'Programming'
+          name: 'Rusty Swain',
+          role: 'Unity Gameplay Programming'
+        },
+        {
+          name: 'TJay Cargle',
+          role: 'Unity UI Programming'
+        },
+        {
+          name: 'Nicolas Riley',
+          role: 'Level and Character Assets/Textures/Level and Character Designs'
+        },
+        {
+          name: 'Khoa Phan',
+          role: 'Level Assets/Materials/Level Designs'
+        },
+        {
+          name: 'John Hughes',
+          role: 'Music and Sound Design'
         },
       ]
     },
+
     {
-      id:"blank",
-      teamName: '[Blank]',
-      projectTitle: 'Project U.S.O.R',
-      githubLink: '#',
-      downloadLink: '#',
-      youTubeID: 'IY8_ucCmQbs',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nunc turpis, volutpat id feugiat eu, auctor ac leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed tortor dolor, rhoncus ut nisl quis, malesuada efficitur ligula',
+      id:"vow",
+      teamName: 'Underrated Vow',
+      projectTitle: 'Word Adventurez',
+      githubLink: 'https://github.com/bearbos/HoFGameJam.git',
+      downloadLink: 'https://drive.google.com/folderview?id=0BzDTL6kkM7d9OWtaRkU5RlVyMVk&usp=sharing_eid&ts=56c48658',
+      youTubeID: 'Uwav8iGlmAk',
+      description: 'Cooperative puzzle game meant to be played alone or with a friend, solving puzzles in each world to move your ship to the next one. Each character is controlled by a joystick and have different abilities so they will need to work together to make it through the game. The controller is just two arcade joysticks, nothing else is needed, so CECO will be able to have small kids with all the varying levels of Cerebral Palsy help the small aliens on their earth vacation!',
       members: [
         {
-          name: 'Joe Slater',
-          role: 'Audio Engineering'
+          name: 'Vishwajeet Kale',
+          role: 'Level Layouts/Presentation'
         },
         {
-          name: 'Rebecca Carroll',
-          role: 'Back-End Dev'
+          name: 'Lexie Rood',
+          role: 'Level Layouts/Presentation'
         },
         {
-          name: 'Jimmy Johnson',
-          role: 'Programming'
+          name: 'Rusty Swain',
+          role: 'Unity Gameplay Programming'
+        },
+        {
+          name: 'TJay Cargle',
+          role: 'Unity UI Programming'
+        },
+        {
+          name: 'Nicolas Riley',
+          role: 'Level and Character Assets/Textures/Level and Character Designs'
+        },
+        {
+          name: 'Khoa Phan',
+          role: 'Level Assets/Materials/Level Designs'
+        },
+        {
+          name: 'John Hughes',
+          role: 'Music and Sound Design'
         },
       ]
     },
+
+
     {
-      id:"blank",
-      teamName: '[Blank]',
-      projectTitle: 'Project U.S.O.R',
-      githubLink: '#',
-      downloadLink: '#',
-      youTubeID: 'IY8_ucCmQbs',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nunc turpis, volutpat id feugiat eu, auctor ac leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed tortor dolor, rhoncus ut nisl quis, malesuada efficitur ligula',
+      id:"left",
+      teamName: 'Leftovers',
+      projectTitle: 'Takeover',
+      githubLink: 'https://drive.google.com/folderview?id=0B7ReMDLGCLolNWl2OUgyeE45c00&usp=sharing',
+      downloadLink: 'https://drive.google.com/open?id=0B7ReMDLGCLolV29VNVA5aDVMZHc',
+      youTubeID: '5XQUeDhWSPg',
+      description: 'Robot third-person shooter, where the player is protecting the city from the opposing robot. The game was meant to give players who have disabilities to have the advantage in playing. This was done by the control scheme planned to use facial recognition and head movement and winking to shoot.',
       members: [
         {
-          name: 'Joe Slater',
-          role: 'Audio Engineering'
+          name: 'Desmon Fields',
+          role: 'Developer'
         },
         {
-          name: 'Rebecca Carroll',
-          role: 'Back-End Dev'
+          name: 'Mike Manahan',
+          role: 'Developer'
         },
         {
-          name: 'Jimmy Johnson',
-          role: 'Programming'
+          name: 'Christopher Gonzalez',
+          role: 'Developer'
+        },
+        {
+          name: 'Chad Fields',
+          role: 'Artist'
+        },
+        {
+          name: 'Daze Davison',
+          role: 'Artist'
+        },
+        {
+          name: 'Javier Tellez',
+          role: 'Developer'
         },
       ]
-    }
+    },
+
+
+
+        {
+          id:"yes",
+          teamName: 'Yes, Kurtis',
+          projectTitle: 'Fish Fish',
+          githubLink: 'https://github.com/Masakurt/HoFPublicRepo.git',
+          downloadLink: 'https://drive.google.com/folderview?id=0B3dh_oNafeeHUDlBbnh3MVdYMHc&usp=sharing',
+          youTubeID: '8uVLmeJGwUs',
+          description: 'Cooperative puzzle game meant to be played alone or with a friend, solving puzzles in each world to move your ship to the next one. Each character is controlled by a joystick and have different abilities so they will need to work together to make it through the game. The controller is just two arcade joysticks, nothing else is needed, so CECO will be able to have small kids with all the varying levels of Cerebral Palsy help the small aliens on their earth vacation!',
+          members: [
+            {
+              name: 'Brittany Ball',
+              role: 'Game Design'
+            },
+            {
+              name: 'Noel Teros',
+              role: 'Game Design'
+            },
+            {
+              name: 'Courtney Stephens',
+              role: 'Art'
+            },
+            {
+              name: 'Chase Maldonado',
+              role: 'Art'
+            },
+            {
+              name: 'Christine Dietz',
+              role: 'Sound Design'
+            },
+            {
+              name: 'Kurtis Vernier',
+              role: 'Programming'
+            },
+            {
+              name: 'Ricky Martinez',
+              role: 'Programming'
+            },
+          ]
+        }
   ];
 
 
